bc9454f82a5e5b7c81b3dbb5f68811fbdf61f4dd
transcendentsky <transcendentsky@gmail.com>
2021-03-07 07:55:41 -0500

    add eval

--- build/lib/tutils/__init__.py
+++ build/lib/tutils/__init__.py
@@ -1,1 +1,13 @@
-from .tutils import *
+# from .tutils import *
+# from . import eval
+# from . import timg
+# from . import tsitk
+# from . import myfunc
+# from . import tlogger
+
+from tutils.tutils import *
+from tutils import eval
+from tutils import timg
+from tutils import tsitk
+from tutils import myfunc
+from tutils import tlogger
---
+++ build/lib/tutils/timg/multi_loader.py
@@ -0,0 +1,4 @@
+import csv
+# import matplotlib.pyplot as plt
+import pylab as plt
+import numpy as np
